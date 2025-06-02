a8fd5270ae5b6a2a283a2d952eb1d44f37810f2b
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-06-02 21:47:46 +0800

    Add files via upload
    destination point inside the main building was added

--- procedural.py
+++ procedural.py
@@ -6,5 +6,25 @@ point_locations = [
     "e.) Main Building"
 ]
 
+main_building_points = [
+    "a.) Main Entrance",
+    "b.) Gymnasium",
+    "c.) Swimming Pool",
+    "d.) Chappel",
+    "e.) Main Academic Building",
+    "f.) Lagoon",
+    "g.) Obelisk",
+    "h.) Ninoy Aquino Learning Resource Center (Library)",
+    "i.) Oval",
+    "j.) Ferry Station",
+]
+
 location_str = "\n".join(point_locations)
-location = input(f"Select your current location:\n \n{location_str}").strip()
+current_location = input(f"{location_str}\n\nSelect your current location: ").strip()
+
+destination = input(f"\n{location_str}\n\nWhere do you want to go?: ").strip()
+
+if destination == "e":
+    destination_points_str = "\n".join(main_building_points)
+    destination_point = input(f"\n{destination_points_str}\n\nSelect your destination point: ").strip()
+    
