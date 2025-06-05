9b3729bd57868ff7ee4063873d494bc8641d1f22
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-06-05 14:29:30 +0800

    distance_per_points
    This show how far is the distance from one point to another (in meter/s)

---
+++ distance_per_points.py
@@ -0,0 +1,15 @@
+#format: (starting, destination, distance in meters)
+[
+    ("Main", "CEA", 750),
+    ("Main", "COC", 650)
+    ("Main", "I-Tech", 1000), 
+    ("Main", "Hasmine", 1300),
+    ("CEA", "COC", 150),
+    ("CEA", "I-Tech", 230),
+    ("CEA", "Hasmine", 800),  # via Pureza to Valencia
+    ("CEA", "Hasmine", 700),  # via Anonas to Valencia
+    ("COC", "I-Tech", 230),
+    ("COC", "Hasmine", 950),  # via Pureza to Valencia
+    ("COC", "Hasmine", 850),  # via Anonas to Valencia
+    ("I-Tech", "Hasmine", 650)
+]
