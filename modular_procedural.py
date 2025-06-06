a3830558a5e5b587081ae106c0ca4b924d153b1c
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-06-06 11:23:21 +0800

    modular_procedural
    revised some areas in the program

--- modular_procedural.py
+++ modular_procedural.py
@@ -1,15 +1,15 @@
 point_locations = {
-    "a": "College of Engineering and Architecture (CEA)",
-    "b": "College of Communication (COC)",
-    "c": "College of Information Technology (I-Tech)",
-    "d": "Hasmine Building",
-    "e": "Main Building"
+    "1": "College of Engineering and Architecture (CEA)",
+    "2": "College of Communication (COC)",
+    "3": "College of Information Technology (I-Tech)",
+    "4": "Hasmine Building",
+    "5": "Main Building"
 }
 
 location_lines = []
 
-for letter, building_name in point_locations.items():
-    line = (f"{letter}.) {building_name}")
+for number, building_name in point_locations.items():
+    line = (f"{number}.) {building_name}")
     location_lines.append(line)
  
 #converts list into a string
@@ -18,12 +18,12 @@ location_str = "\n".join(location_lines)
 def get_location_input(prompt):
     while True:
         print(location_str)
-        choice = input(f"{prompt} (enter letter only): ").strip().lower()
+        choice = input(f"{prompt} (enter number only): ").strip().lower()
         #check if the letter is in the point_location keys
         if choice in point_locations:
             return choice
         else:
-            print("Invalid input. Please choose a valid letter.\n")
+            print("Invalid input. Please choose a valid option.\n")
 
 #asks user for current location and destination
 current_location = get_location_input("\nSelect your current location")
